e222ba5149ddb6e54a617bfad84ec74fbbbb2e30
Robin Biegel <robin.biegel@brogit.de>
2024-10-20 15:52:06 +0200

    Update wifi

--- app/static/wifi.js
+++ app/static/wifi.js
@@ -133,7 +133,8 @@ $(document).ready(function() {
                     $('a.device-url').attr('href', url).text(url);
                     $('#connect-step-wait-for-reconnect').find('section.wait').slideUp(400, function() {
                         $('#connect-step-wait-for-reconnect').find('section.success').fadeIn(400, function() {
-                            // window.location.href = url;
+                            alert(url);
+                            window.location.replace(url);
                         });
                     });
                 },
