dd1deb15b68835fff8d8ca9d0e8050126ce0cae4
Robin Biegel <robin.biegel@brogit.de>
2024-10-20 16:51:57 +0200

    Update AP

--- change_hostname.sh
+++ change_hostname.sh
@@ -4,3 +4,5 @@ mac_address=$(ip link show | awk '/ether/ {print $2; exit}' | sed 's/://g' | tai
 new_hostname="bMediaPi-$mac_address"
 
 sudo hostnamectl set-hostname "$new_hostname"
+
+sudo sed -i.bak "s/127.0.1.1.*/127.0.1.1 $new_hostname/g" /etc/hosts
