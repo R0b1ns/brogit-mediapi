254d20aff3687e119fe5bcc6fe764ecfcb13dbbc
Robin Biegel <robin.biegel@brogit.de>
2024-10-20 16:44:53 +0200

    Update AP

--- ap.sh
+++ ap.sh
@@ -2,3 +2,17 @@ sudo apt-get install -y hostapd dnsmasq
 
 sudo systemctl stop hostapd
 sudo systemctl stop dnsmasq
+
+# Konfiguriere hostapd
+cat <<EOL | sudo tee /etc/hostapd/hostapd.conf
+interface=wlan0
+driver=nl80211
+ssid=$(hostname)-AP
+hw_mode=g
+channel=6
+wmm_enabled=0
+macaddr_acl=0
+auth_algs=1
+ignore_broadcast_ssid=0
+wpa=0
+EOL
