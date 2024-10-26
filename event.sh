206898e923601f0b9db949e7ffc311b816dca208
Robin Biegel <robin.biegel@brogit.de>
2024-10-26 13:11:50 +0200

    Update eventhandler

--- event.sh
+++ event.sh
@@ -2,12 +2,9 @@
 
 source "load_config.sh"
 
-echo "${config[locale]}"
-echo "${translations[bt_connect_text]}"
-
 if [[ "$1" == "bt" ]]; then
   echo "Bluetooth Event"
-  LOCALE="${config[locale]}" CONNECT_TEXT="${translations[bt_connect_text]}" CONFIG=$config bash ./bt/event.sh "${@:2}"
+  LOCALE="${config[locale]}" CONNECT_TEXT="${translations[bt_connect_text]}" ERROR_DEVICES_EMPTY="${translations[bt_connect_text]}" bash ./bt/event.sh "${@:2}"
 elif [[ "$1" == "wifi" ]]; then
   echo "Wifi Event"
 else
