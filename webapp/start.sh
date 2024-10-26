0be24d4d1593c4be7449c44d0991e62367384298
Robin Biegel <robin.biegel@brogit.de>
2024-10-26 20:23:46 +0200

    Make sure skript can be executed from everywhere

--- webapp/start.sh
+++ webapp/start.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 if [ ! -d ".venv" ]; then
     echo "Create virtual environment..."
-    python3 -m venv .venv
+    python3 -m venv "$SCRIPT_DIR/.venv"
 fi
 
 source ./.venv/bin/activate
 
 echo "Install dependencies ..."
-pip3 install -r requirements.txt
+pip3 install -r "$SCRIPT_DIR/requirements.txt"
 
 echo "Run ..."
-source ./.venv/bin/activate
-python3 ./app.py
+source "$SCRIPT_DIR/.venv/bin/activate"
+python3 "$SCRIPT_DIR/app.py"
