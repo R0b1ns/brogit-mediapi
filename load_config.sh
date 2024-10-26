99c84b964fd6a6563a3541756576345265a5e6c9
Robin Biegel <robin.biegel@brogit.de>
2024-10-26 12:26:02 +0200

    Fix: bad array subscript

--- load_config.sh
+++ load_config.sh
@@ -3,21 +3,26 @@
 # Path to config file
 CONFIG_FILE="mediapi.conf"
 
-# Declare associative Array
-declare -A config
-
 # Read config
-if [[ -f "$CONFIG_FILE" ]]; then
-    while IFS='=' read -r key value; do
-        # Remove whitespace
-        key=$(echo "$key" | xargs)
-        value=$(echo "$value" | xargs)
-        config["$key"]="$value"
-    done < "$CONFIG_FILE"
-else
-    echo "Config file $CONFIG_FILE does not exist."
-    exit 1
-fi
+load_config() {
+    # Declare associative Array
+    declare -A config
+    if [[ -f "$CONFIG_FILE" ]]; then
+        while IFS='=' read -r key value; do
+            # Remove whitespace
+            key=$(echo "$key" | xargs)
+            value=$(echo "$value" | xargs)
+
+            # Only add valid kv
+            if [[ -n "$key" && -n "$value" ]]; then
+                config["$key"]="$value"
+            fi
+        done < "$CONFIG_FILE"
+    else
+        echo "Config file $CONFIG_FILE does not exist."
+        exit 1
+    fi
+}
 
 load_translations() {
     local lang_file="$1"
@@ -27,6 +32,8 @@ load_translations() {
         translations["$key"]="$value"
     done < "$lang_file"
 }
+
+load_config
 
 # TODO: Dynamic load by locale
 load_translations "locale/strings_de.conf"
