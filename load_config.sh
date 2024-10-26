86a5cf1b2b4e9c3569561fa13927080c8b2ad933
Robin Biegel <robin.biegel@brogit.de>
2024-10-26 12:41:11 +0200

    tset

--- load_config.sh
+++ load_config.sh
@@ -3,18 +3,41 @@
 # Path to config file
 CONFIG_FILE="mediapi.conf"
 
-# Declare associative Array
-declare -A config
-
 # Read config
-if [[ -f "$CONFIG_FILE" ]]; then
+load_config() {
+    # Declare associative Array
+    declare -A config
+    if [[ -f "$CONFIG_FILE" ]]; then
+        while IFS='=' read -r key value; do
+            # Remove whitespace
+            key=$(echo "$key" | xargs)
+            value=$(echo "$value" | xargs)
+
+            # Only add valid kv
+            if [[ -n "$key" && -n "$value" ]]; then
+                config["$key"]="$value"
+            fi
+        done < "$CONFIG_FILE"
+    else
+        echo "Config file $CONFIG_FILE does not exist."
+        exit 1
+    fi
+}
+
+load_translations() {
+    local lang_file="$1"
+    declare -gA translations
+
     while IFS='=' read -r key value; do
-        # Remove whitespace
-        key=$(echo "$key" | xargs)
-        value=$(echo "$value" | xargs)
-        config["$key"]="$value"
-    done < "$CONFIG_FILE"
-else
-    echo "Config file $CONFIG_FILE does not exist."
-    exit 1
-fi
+        translations["$key"]="$value"
+    done < "$lang_file"
+}
+
+load_config
+
+# TODO: Dynamic load by locale
+load_translations "locale/strings_de.conf"
+
+echo "Host: ${config[host]}"
+echo "Port: ${config[port]}"
+echo "Willkommen: ${translations[bt_connect_text]}"
