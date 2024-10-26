4a77e752225a5b270bdb1580a83b7fdbe65c4ef9
Robin Biegel <robin.biegel@brogit.de>
2024-10-26 15:55:31 +0200

    Implemented event log

--- event.sh
+++ event.sh
@@ -2,6 +2,11 @@
 
 source "load_config.sh"
 
+mkdir -p "${config[log_dir]}"
+LOG_FILE="${config[log_dir]}/event.log"
+
+echo "$@" >> LOG_FILE
+
 if [[ "$1" == "bt" ]]; then
   echo "Bluetooth Event"
   LOCALE="${config[locale]}" CONNECT_TEXT="${translations[bt_connect_text]}" ERROR_DEVICES_EMPTY="${translations[error_devices_empty]}" bash ./bt/event.sh "${@:2}"
