0f457aef501b7d6e154b8064865638b6f4d2265f
Robin Biegel <robin.biegel@brogit.de>
2024-10-26 12:38:51 +0200

    tset

--- load_config.sh
+++ load_config.sh
@@ -7,34 +7,14 @@ CONFIG_FILE="mediapi.conf"
 declare -A config
 
 # Read config
-load_config() {
-    if [[ -f "$CONFIG_FILE" ]]; then
-        while IFS='=' read -r key value; do
-            # Remove whitespace
-            key=$(echo "$key" | xargs)
-            value=$(echo "$value" | xargs)
-
-            # Only add valid kv
-            if [[ -n "$key" && -n "$value" ]]; then
-                config["$key"]="$value"
-            fi
-        done < "$CONFIG_FILE"
-    else
-        echo "Config file $CONFIG_FILE does not exist."
-        exit 1
-    fi
-}
-
-load_translations() {
-    local lang_file="$1"
-    declare -gA translations
-
+if [[ -f "$CONFIG_FILE" ]]; then
     while IFS='=' read -r key value; do
-        translations["$key"]="$value"
-    done < "$lang_file"
-}
-
-load_config
-
-# TODO: Dynamic load by locale
-load_translations "locale/strings_de.conf"
+        # Remove whitespace
+        key=$(echo "$key" | xargs)
+        value=$(echo "$value" | xargs)
+        config["$key"]="$value"
+    done < "$CONFIG_FILE"
+else
+    echo "Config file $CONFIG_FILE does not exist."
+    exit 1
+fi
