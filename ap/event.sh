3bc557c7a8f86f009053d269cb13db90b433eeb5
Robin Biegel <robin.biegel@brogit.de>
2024-11-09 17:18:06 +0100

    Webapp connect

--- ap/event.sh
+++ ap/event.sh
@@ -16,22 +16,21 @@ source "$PROJECT_ROOT_PATH/load_config.sh"
 
 MODULE_NAME="${config[project_name]}_ap"
 PROJECT_USER="${config[project_user]}"
+WIFI_INTERFACE="${config[wifi_interface]}"
 
 LOG_DIR="${config[log_dir]}/${config[project_name]}"
 LOG_FILE=$(create_log_with_rotation "$LOG_DIR" "$MODULE_NAME")
 
 # Log-Nachricht hinzufügen
 log_message "$LOG_FILE" "Wifi dispatcher event: $1, $2"
-log_message "$LOG_FILE" "Was los - $WIFI_INTERFACE"
+log_message "$LOG_FILE" "Controls: '$WIFI_INTERFACE'"
 
 
 
 if [[ "$1" == "$WIFI_INTERFACE" && "$2" == "up" ]]; then
-  log_message "$LOG_FILE" "Was los2"
   log_message "$LOG_FILE" "Wifi Up Cancel AP Delayed start"
   AP_START_DELAY_AFTER_DISCONNECT="$AP_START_DELAY_AFTER_DISCONNECT" bash "$SCRIPT_DIR/delayed_start.sh cancel"
 elif [[ "$1" == "$WIFI_INTERFACE" && "$2" == "down" ]]; then
-  log_message "$LOG_FILE" "Was los3"
   log_message "$LOG_FILE" "Wifi Down - Start AP delayed in $AP_START_DELAY_AFTER_DISCONNECT minutes"
   AP_START_DELAY_AFTER_DISCONNECT="$AP_START_DELAY_AFTER_DISCONNECT" bash "$SCRIPT_DIR/delayed_start.sh"
 fi
