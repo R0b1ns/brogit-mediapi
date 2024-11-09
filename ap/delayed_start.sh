1da8eb04224921805290b7b4b188c78b6f6acf3a
Robin Biegel <robin.biegel@brogit.de>
2024-11-09 17:54:58 +0100

    Webapp connect

--- ap/delayed_start.sh
+++ ap/delayed_start.sh
@@ -5,34 +5,51 @@
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-echo "Delayed start AP"
+PROJECT_RELATIVE_ROOT="../"
+PROJECT_ROOT_PATH="$(cd "$SCRIPT_DIR/$PROJECT_RELATIVE_ROOT" && pwd)"
 
-PID_FILE="$SCRIPT_DIR/ap_delayed_start.pid"
-CANCEL_FLAG_FILE="$SCRIPT_DIR/ap_cancel_start.flag"
+MODULE_DIR="$SCRIPT_DIR"
+
+source "$PROJECT_ROOT_PATH/helper.sh"
+source "$PROJECT_ROOT_PATH/load_config.sh"
+
+MODULE_NAME="${config[project_name]}_ap"
+PROJECT_USER="${config[project_user]}"
+WIFI_INTERFACE="${config[wifi_interface]}"
+
+LOG_DIR="${config[log_dir]}/${config[project_name]}"
+LOG_FILE=$(create_log_with_rotation "$LOG_DIR" "$MODULE_NAME")
+
+log_message "$LOG_FILE" "Delayed start :: Welcome"
+
+PID_FILE="$MODULE_DIR/ap_delayed_start.pid"
+CANCEL_FLAG_FILE="$MODULE_DIR/ap_cancel_start.flag"
 
 AP_START_DELAY_AFTER_DISCONNECT="${AP_START_DELAY_AFTER_DISCONNECT:-10}"
 DELAY_SECONDS=$((AP_START_DELAY_AFTER_DISCONNECT * 60))
 
 if iwgetid -r "$WIFI_INTERFACE" > /dev/null 2>&1; then
-    echo "Info: $WIFI_INTERFACE already connected. Exit"
+    log_message "$LOG_FILE" "Delayed start :: Info: $WIFI_INTERFACE already connected. Exit"
     exit 0
 fi
 
 if [[ "$1" == "cancel" ]]; then
+  log_message "$LOG_FILE" "Delayed start :: Cancel"
   if [ -f "$PID_FILE" ]; then
     PID=$(cat "$PID_FILE")
     if ps -p "$PID" > /dev/null; then
       echo "Delayed start runs"
+      log_message "$LOG_FILE" "Delayed start :: Active process exists"
       touch "$CANCEL_FLAG_FILE"
-      echo "Info: Created Cancel flag"
+      log_message "$LOG_FILE" "Delayed start :: Created Cancel flag"
       exit 0
     else
-      echo "Warning: PID File exists but no process running. PID file will be removed. Exit"
+      log_message "$LOG_FILE" "Delayed start :: Warning: PID File exists but no process running. PID file will be removed. Exit"
       rm "$PID_FILE"
       exit 1
     fi
   else
-    echo "Warning: No delayed start running. Exit"
+    log_message "$LOG_FILE" "Delayed start :: Warning: No delayed start running. Exit"
     exit 0
   fi
 fi
@@ -41,16 +58,17 @@ if [ -f "$PID_FILE" ]; then
   # Lockfile already exists
   PID=$(cat "$PID_FILE")
   if ps -p "$PID" > /dev/null; then
-    echo "Skript is already running PID=$PID. Exit"
+    log_message "$LOG_FILE" "Delayed start :: Skript is already running PID=$PID. Exit"
     exit 1
   else
-    echo "Warning: PID File exists but no process running. PID file will be removed."
+    log_message "$LOG_FILE" "Delayed start :: Warning: PID File exists but no process running. PID file will be removed."
     rm "$PID_FILE"
   fi
 fi
 
 # Save PID to File
 echo $$ > "$PID_FILE"
+log_message "$LOG_FILE" "Delayed start :: Skript is already running PID=$PID. Exit"
 
 # Remove PID File when skript is canceled
 trap ctrl_c INT
@@ -61,17 +79,17 @@ ctrl_c () {
 }
 
 if [ -f "$CANCEL_FLAG_FILE" ]; then
-  echo "Stop file exists before even tried.. Must be a leftover. Lets remove it.."
+  log_message "$LOG_FILE" "Delayed start :: Stop file exists before even tried.. Must be a leftover. Lets remove it.."
   rm $CANCEL_FLAG_FILE
 fi
 
-echo "Start delayed in $DELAY_SECONDS seconds..."
+log_message "$LOG_FILE" "Delayed start :: Start delayed in $DELAY_SECONDS seconds..."
 
 # Delayed start. If stop file appears, we exit
 for ((i=0; i<600; i++)); do
   if [ -f "$CANCEL_FLAG_FILE" ]; then
-    echo "Info: Delayed start is stopped!"
-    echo "Warning: Remove stop flag and pid file"
+    log_message "$LOG_FILE" "Delayed start :: Info: Delayed start is stopped!"
+    log_message "$LOG_FILE" "Delayed start :: Warning: Remove stop flag and pid file"
     rm "$CANCEL_FLAG_FILE"
     rm "$PID_FILE"
     exit 0
@@ -79,5 +97,5 @@ for ((i=0; i<600; i++)); do
   sleep 1
 done
 
-echo "Start Access Point ..."
-bash "$SCRIPT_DIR/start.sh"
+log_message "$LOG_FILE" "Delayed start :: Start Access Point ..."
+bash "$MODULE_DIR/start.sh"
