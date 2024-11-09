e4e65df59c68283c55e14bbe082deb5f7a4a9157
Robin Biegel <robin.biegel@brogit.de>
2024-11-09 18:51:11 +0100

    Webapp connect

--- ap/event.sh
+++ ap/event.sh
@@ -28,7 +28,7 @@ log_message "$LOG_FILE" "Event :: Wifi dispatcher event (Controls '$WIFI_INTERFA
 
 if [[ "$1" == "$WIFI_INTERFACE" && "$2" == "up" ]]; then
   log_message "$LOG_FILE" "Event :: Cancel Delayed start (Wifi Up)"
-  AP_START_DELAY_AFTER_DISCONNECT="$AP_START_DELAY_AFTER_DISCONNECT" bash "$MODULE_DIR/delayed_start.sh cancel"
+  AP_START_DELAY_AFTER_DISCONNECT="$AP_START_DELAY_AFTER_DISCONNECT" bash "$MODULE_DIR/delayed_start.sh" cancel
 elif [[ "$1" == "$WIFI_INTERFACE" && "$2" == "down" ]]; then
   log_message "$LOG_FILE" "Event :: Start AP delayed in $AP_START_DELAY_AFTER_DISCONNECT minutes (Wifi Down)"
   AP_START_DELAY_AFTER_DISCONNECT="$AP_START_DELAY_AFTER_DISCONNECT" bash "$MODULE_DIR/delayed_start.sh"
