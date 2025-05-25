fef6b469e94dbe78dd48471674f4c30394f0bffb
R0b1ns <r0b1ns@brogit.de>
2025-05-25 16:49:50 +0200

    Print only on debug for locale info

--- webapp/app/core/locale/models.py
+++ webapp/app/core/locale/models.py
@@ -1,12 +1,16 @@
+import logging
+
 from flask import current_app, request, g, has_request_context
 
 
 # @babel.localeselector
 def get_locale():
-    print("get_locale")
+    default_locale = 'en'
+
     # if a user is logged in, use the locale from the user settings
     user = getattr(g, 'user', None)
     if user is not None:
+        logging.debug(f"get_locale: User context = {user.locale}")
         return user.locale
     # otherwise try to guess the language from the user accept
     # header the browser transmits. The best match wins.
@@ -15,10 +19,12 @@ def get_locale():
     #  But it does only once before request context
     #  It will get fired on every request if _() is used in the request, but not for templates
     if has_request_context():
-        print("Request context")
-        return request.accept_languages.best_match(current_app.config['LANGUAGES'])
-    # return 'en'
-    return 'de'
+        l = request.accept_languages.best_match(current_app.config['LANGUAGES'])
+        logging.debug(f"get_locale: Request context = {l}")
+        return l
+
+    logging.debug(f"get_locale: Default = {default_locale}")
+    return default_locale
 
 def get_timezone():
     user = getattr(g, 'user', None)
