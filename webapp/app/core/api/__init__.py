b19a75bdbdcb92e23e98b1e3e1a0f405fd6d053a
R0b1ns <r0b1ns@brogit.de>
2025-06-16 22:58:10 +0200

    Enforce JSON on API routes

--- webapp/app/core/api/__init__.py
+++ webapp/app/core/api/__init__.py
@@ -1,5 +1,12 @@
-from flask import Blueprint
+from flask import Blueprint, request, abort
 
 api_bp = Blueprint('api', __name__, url_prefix='/api')
+
+@api_bp.before_request
+def enforce_json():
+    if request.method in ("POST", "PUT", "PATCH"):
+        if not request.is_json:
+            abort(400, description="JSON expected")
+
 
 from . import settings
