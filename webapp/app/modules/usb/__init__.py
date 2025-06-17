0842d52607fef3356749ed47082709477359afb8
R0b1ns <r0b1ns@brogit.de>
2025-06-17 16:36:33 +0200

    Inital route work

--- webapp/app/modules/usb/__init__.py
+++ webapp/app/modules/usb/__init__.py
@@ -1,11 +1,11 @@
 from flask import Blueprint
 
-usb_bp = Blueprint(
+module_bp = Blueprint(
     'usb',
     __name__,
     template_folder='templates',
     static_folder='static',
-    # url_prefix='/network'
+    url_prefix='/usb'
 )
 
-# from . import routes
+from . import routes
---
+++ webapp/app/modules/usb/routes.py
@@ -0,0 +1,59 @@
+from flask import request, jsonify
+
+from app.modules.usb import module_bp
+
+
+@module_bp.route('/api/settings/network-share', methods=['POST'])
+def update_settings_network_share():
+    data = request.get_json()
+    enabled = data.get('enabled')
+
+    if enabled is None:
+        return jsonify(error="Missing fields"), 400
+
+    bool_to_str = {
+        0: 'Disabled',
+        1: 'Enabled'
+    }
+
+    # TODO: Implement backend
+    if not False:
+        return jsonify(message="Unable to change configuration of 'System-Update'"), 400
+
+    return jsonify(message=f"System-Update: {bool_to_str.get(enabled)}")
+
+@module_bp.route('/api/settings/configure', methods=['POST'])
+def usb_settings():
+    data = request.get_json()
+    enable_audio = data.get('audio')
+
+    # TODO: Implement backend
+    if not False:
+        return jsonify(message="Not implemented"), 400
+
+    return jsonify(message=f"Success")
+
+@module_bp.route('/api/settings/protect', methods=['POST'])
+def usb_settings():
+    data = request.get_json()
+    enabled = data.get('enabled')
+    username = data.get('username')
+    password = data.get('password')
+
+    # TODO: Implement backend
+    if not False:
+        return jsonify(message="Not implemented"), 400
+
+    return jsonify(message=f"Success")
+
+@module_bp.route('/api/settings/ports', methods=['POST'])
+def usb_configure_ports():
+    data = request.get_json()
+    enabled = data.get('enabled')
+    port = data.get('port')
+
+    # TODO: Implement backend
+    if not False:
+        return jsonify(message="Not implemented"), 400
+
+    return jsonify(message=f"Success")
