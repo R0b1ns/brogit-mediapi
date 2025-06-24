dd45c081498aa177762cebcd2e221bc6223f9d30
R0b1ns <r0b1ns@brogit.de>
2025-06-24 16:09:08 +0200

    Progress on bluetooth module

--- webapp/app/modules/bluetooth/BluetoothModule.py
+++ webapp/app/modules/bluetooth/BluetoothModule.py
@@ -1,3 +1,6 @@
+import logging
+
+from app.lib.Backend import Backend
 from app.lib.ModuleInterface import ModuleInterface
 from app.lib.env_config import EnvConfig
 
@@ -6,14 +9,63 @@ class BluetoothModule(ModuleInterface):
     def __init__(self, config):
         super().__init__(config)
         module_config = config.get('bluetooth')
+        self.__module_config = module_config
+
         self.env_config = EnvConfig(module_config.get('config_path'))
         for k in module_config.get('deploy_on'):
             self.env_config.register_callback(k, self.deploy)
 
+        self.device_class_mapping = module_config.get('device_class_mapping')
+
+    def set(self, key, value):
+        # Keys and validator lambda
+        valid_options = {
+            'DEVICE_CLASS': lambda v: v in self.__module_config.get('device_class_mapping'),
+            'DISCOVERABLE': lambda v: v in (True, False),
+            # TODO: get_audio_devices not just has names. Improve that
+            'AUDIO_DEVICE': lambda v: v in Backend().audio.get_audio_devices(),
+            'SOUND_ENABLED': lambda v: v in (True, False),
+            'VOICE_ENABLED': lambda v: v in (True, False),
+        }
+
+        option_mapping = {
+            'DISCOVERABLE': {
+                True: 'on',
+                False: 'off'
+            },
+            'SOUND_ENABLED': {
+                True: 'True',
+                False: 'False'
+            },
+            'VOICE_ENABLED': {
+                True: 'True',
+                False: 'False'
+            },
+        }
+
+        validator = valid_options.get(key)
+
+        if not validator:
+            logging.debug(f'No validator for key={key}')
+            return None
+
+        if validator(value):
+            transformation = option_mapping.get(key)
+
+            if transformation:
+                self.env_config[key] = transformation.get(value)
+            else:
+                self.env_config[key] = value
+
+            return True
+
+        return False
+
     def get_config(self):
-        # TODO: Remove that, just for dev purpose
-        self.env_config['DISCOVERABLE'] = "off"
         return self.env_config
+
+    def get_device_class_mapping(self):
+        return self.device_class_mapping
 
     def install(self):
         pass
--- webapp/app/modules/bluetooth/routes.py
+++ webapp/app/modules/bluetooth/routes.py
@@ -1,4 +1,4 @@
-from flask import request, jsonify
+from flask import request, jsonify, current_app
 
 from app.lib.Backend import Backend
 from app.modules.bluetooth import module_bp
@@ -6,7 +6,8 @@ from app.modules.bluetooth import module_bp
 
 @module_bp.route('/api/test', methods=['GET'])
 def bluetooth_test():
-    r = Backend().bluetooth.read()
+    print(current_app.config)
+    r = Backend().bluetooth.get_config()
     return jsonify(message=r)
 
 
@@ -31,3 +32,36 @@ def bluetooth_audio_device():
         return jsonify(message="Not implemented"), 400
 
     return jsonify(message=f"Success")
+
+@module_bp.route('/api/settings', methods=['POST'])
+def change_bluetooth_settings():
+    data = request.get_json()
+
+    if not data:
+        return jsonify(message="No data"), 400
+
+    result = {}
+
+    for k, v in data.items():
+        result.update({
+            k: Backend().bluetooth.set(k, v)
+        })
+
+    # for k, f in valid_options.items():
+    #     value = data.get(k)
+    #
+    #     if value is not None:
+    #         result.update({
+    #             k: False
+    #         })
+    #
+    #     if f(value):
+    #         Backend().bluetooth.set(k, value)
+    #         result[k] = True
+    #
+    # if not result:
+    #     return jsonify(message=f"Wrong keys. Valid keys: {valid_options.keys()}"), 400
+
+
+
+    return jsonify(response=result)
