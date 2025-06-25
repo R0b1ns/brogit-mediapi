e9fb8482e0a59b3bdcc4e74fa99bb0791ae91147
R0b1ns <r0b1ns@brogit.de>
2025-06-25 12:06:44 +0200

    Better uninstall with stopping of service

--- bt/uninstall.sh
+++ bt/uninstall.sh
@@ -11,12 +11,18 @@ source "$SCRIPT_DIR/config.env"
 
 AUDIO_DEVICE="${AUDIO_DEVICE:-hci0}"
 
-sudo rm /etc/systemd/system/bt-agent@.service
-sudo rm /etc/udev/rules.d/99-bluetooth-connect.rules
-
 sudo systemctl disable bt-agent@${AUDIO_DEVICE}.service
-sudo systemctl disable bluealsa
-sudo systemctl disable bluetooth
+sudo systemctl stop bt-agent@${AUDIO_DEVICE}.service
+
+sudo rm /etc/systemd/system/bt-agent@.service
+
 sudo systemctl daemon-reload
 
 cp /etc/bluetooth/main.conf.bak /etc/bluetooth/main.conf
+sudo rm /etc/udev/rules.d/99-bluetooth-connect.rules
+
+sudo systemctl disable bluealsa
+sudo systemctl disable bluetooth
+
+sudo systemctl restart bluealsa
+sudo systemctl restart bluetooth
