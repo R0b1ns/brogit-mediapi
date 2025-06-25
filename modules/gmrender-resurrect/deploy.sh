330bab03122df073be8e6e0e91844872d7ffbbee
R0b1ns <r0b1ns@brogit.de>
2025-06-25 19:02:44 +0200

    No fallback for DEVICE_NAME

--- modules/gmrender-resurrect/deploy.sh
+++ modules/gmrender-resurrect/deploy.sh
@@ -9,7 +9,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/config.env"
 
 # Default values
-DEVICE_NAME="${DEVICE_NAME:-DLNA-Renderer}"
+DEVICE_NAME="${DEVICE_NAME:-}"
 INITIAL_VOLUME_DB="${INITIAL_VOLUME_DB:--10}"
 UPNP_UUID="$(ip link show | awk '/ether/ {print "salt:)-" $2}' | head -1 | md5sum | awk '{print $1}')"
 
