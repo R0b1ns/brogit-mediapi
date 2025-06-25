6fe8a051ecbe59b4244f9816777a5a40d2a547d6
R0b1ns <r0b1ns@brogit.de>
2025-06-25 14:10:34 +0200

    Fix BT Service

--- bt/deploy.sh
+++ bt/deploy.sh
@@ -19,7 +19,6 @@ DEVICE_NAME="${DEVICE_NAME:-}"
 # Set DEVICE_NAME_STR only if DEVICE_NAME is not empty
 if [[ -n "$DEVICE_NAME" ]]; then
   DEVICE_NAME_STR="Name = $DEVICE_NAME"
-  DEVICE_NAME_SERVICE_STR="ExecStartPre=/usr/bin/bluetoothctl system-alias \"$DEVICE_NAME\""
 else
   DEVICE_NAME_STR=""
 fi
@@ -70,7 +69,7 @@ After=bluetooth.service
 
 [Service]
 ExecStartPre=/usr/bin/bluetoothctl discoverable $DISCOVERABLE
-$DEVICE_NAME_SERVICE_STR
+ExecStartPre=/usr/bin/bluetoothctl system-alias "$DEVICE_NAME"
 ExecStartPre=/bin/hciconfig %I piscan
 ExecStartPre=/bin/hciconfig %I sspmode 1
 ExecStart=/usr/bin/bt-agent --capability=NoInputNoOutput
