76aff861f45e42b2ce48dce698a8effd910f954d
R0b1ns <r0b1ns@brogit.de>
2025-06-27 01:14:23 +0200

    Set webapp as www-data user

--- webapp/install.sh
+++ webapp/install.sh
@@ -10,5 +10,8 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 # Deploy and install dependencies
 "$SCRIPT_DIR/deploy.sh"
 
+# Grant rights
+chown -R www-data:www-data "$SCRIPT_DIR"
+
 # Install service
 "$SCRIPT_DIR/scripts/install_service.sh"
