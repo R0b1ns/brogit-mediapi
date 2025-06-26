44996a33fc1ef90f7dec86e9f7ce80960be2493d
R0b1ns <r0b1ns@brogit.de>
2025-06-27 01:49:59 +0200

    No cache for pip install

--- webapp/deploy.sh
+++ webapp/deploy.sh
@@ -5,4 +5,4 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/.venv/bin/activate"
 
 echo "Install pip dependencies ..."
-"$SCRIPT_DIR/.venv/bin/pip" install -r "$SCRIPT_DIR/requirements.txt"
+"$SCRIPT_DIR/.venv/bin/pip" install --no-cache-dir -r "$SCRIPT_DIR/requirements.txt"
