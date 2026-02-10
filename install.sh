8170d27e577c810aa0847f3de5de47eba9d402fb
brogit <r0b1ns@brogit.de>
2026-02-10 19:35:27 +0100

    Make install compatible with web app

--- install.sh
+++ install.sh
@@ -7,19 +7,24 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 chown -R :www-data "$SCRIPT_DIR"
 
+### NOTE: ###
+# Its deprecated now to install everything modular. So therefore we commented that out.
+# You can still install all modules via the webapp. This is the way it should go!
+###
+
 # modules
 # TODO: Change permissions in module
-chmod g+r "$SCRIPT_DIR/modules/gmrender-resurrect/config.env"
-chmod g+r "$SCRIPT_DIR/modules/shairport-sync/config.env"
+# chmod g+r "$SCRIPT_DIR/modules/gmrender-resurrect/config.env"
+# chmod g+r "$SCRIPT_DIR/modules/shairport-sync/config.env"
 
 # bt
-chown -R :www-data "$SCRIPT_DIR/bt"
-chmod +x "$SCRIPT_DIR/bt/install.sh"
+# chown -R :www-data "$SCRIPT_DIR/bt"
+# chmod +x "$SCRIPT_DIR/bt/install.sh"
 # bash "$SCRIPT_DIR/bt/install.sh"
 
 # ap
-chown -R :www-data "$SCRIPT_DIR/ap"
-chmod +x "$SCRIPT_DIR/ap/install.sh"
+# chown -R :www-data "$SCRIPT_DIR/ap"
+# chmod +x "$SCRIPT_DIR/ap/install.sh"
 # bash "$SCRIPT_DIR/ap/install.sh"
 
 # webapp
