cd7d789ccfafdb052a19d49f1a149263da7709cb
R0b1ns <r0b1ns@brogit.de>
2026-02-11 18:52:35 +0100

    Improove uninstall

--- modules/shairport-sync/uninstall.sh
+++ modules/shairport-sync/uninstall.sh
@@ -6,8 +6,22 @@
 systemctl disable shairport-sync
 systemctl stop shairport-sync
 
+BIN_PATH="$(which shairport-sync 2>/dev/null)"
+
+if [ -z "$BIN_PATH" ]; then
+  echo "shairport-sync not found."
+  exit 0
+fi
+
+echo "Found at: $BIN_PATH"
+
+# Remove binary
+sudo rm -f "$BIN_PATH"
+
+echo "Removed."
+
 # WiFi Power Management on (Instant but not after reboot)
-iwconfig wlan0 power on
+# iwconfig wlan0 power on
 
 # NetworkManager (For future connection, also after reboot) (2=disabled, 3=enabled)
-sudo nmcli connection modify preconfigured wifi.powersave 3
+# sudo nmcli connection modify preconfigured wifi.powersave 3
