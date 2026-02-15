b8dc9f4ec5badd88de980d8cb540a91e31203fc9
R0b1ns <r0b1ns@brogit.de>
2026-02-15 20:24:47 +0100

    Added Trigger Callback functionality

--- webapp/app/lib/ModuleInterface.py
+++ webapp/app/lib/ModuleInterface.py
@@ -1,6 +1,9 @@
+# Author: Robin Biegel
 # Version: 2026.02.10
 
 class ModuleInterface:
+    global_callbacks = {}
+
     def __init__(self, config = None):
         pass
 
@@ -18,3 +21,23 @@ class ModuleInterface:
 
     def uninstall(self, confirm: bool = False) -> bool:
         raise NotImplementedError()
+
+    def on(self, event_name: str, callback):
+        """
+        Register a callback to be executed when an event is triggered.
+        """
+        self.__class__.global_callbacks[event_name].append(callback)
+
+    def trigger(self, event_name: str, event_data = None):
+        """
+        Execute registered callbacks for events
+        """
+        result = []
+
+        for callback in self.__class__.global_callbacks[event_name]:
+            try:
+                result.append(callback(event_data))
+            except Exception as e:
+                result.append(f"Failed to execute callback: {str(e)}")
+
+        return result
