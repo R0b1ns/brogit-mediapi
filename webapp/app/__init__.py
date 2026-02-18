dd40cea619229f957f4f9d4079d86f7bf0818232
R0b1ns <r0b1ns@brogit.de>
2026-02-18 18:24:59 +0100

    Debug

--- webapp/app/__init__.py
+++ webapp/app/__init__.py
@@ -58,10 +58,7 @@ def create_app():
     # Basic Logging configuration
     # Not possible basic_config an only be executed once. And here flask already did it
     # setup_logging(log_level.get(config['app'].get('LOGGING', 'info'), logging.INFO))
-    logging.getLogger().setLevel(logging.DEBUG)
-    app.logger.setLevel(logging.DEBUG)
-    logging.debug("Das wird nicht ausgegeben.")
-    app.logger.debug("Das wird ausgegeben")
+    logging.getLogger().setLevel(log_level.get(config['app'].get('LOGGING', 'info'), logging.INFO))
 
     app.logger.info(f"Cors Allowed Origins: { cors_allowed_origins }")
 
